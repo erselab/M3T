b66f1d4f241e40cfcc33735867b909f8f23338d9
kristian Hajny <kristian.hajny@stonybrook.edu>
2024-08-14 22:54:54 -0400

    Improved to account for domain CRS, fix state border issues with mask, and include timings.

--- src/NLCD_fractions_by_state.R
+++ src/NLCD_fractions_by_state.R
@@ -19,7 +19,7 @@
 #'  saved for use in the `Wastewater` function. The total area of the two land
 #'  cover types for each state is saved as a csv.
 #'
-#'@param nlcd_file Character providing the full filepath to the National Land
+#'@param NLCD_file Character providing the full filepath to the National Land
 #'  Cover Database.  This data is available at \url{https://www.mrlc.gov/data}. This
 #'  is a geotif of 30 m land cover data for the Continental United States where
 #'  different values represent the different land cover classifications.  There
@@ -40,7 +40,7 @@
 #'  and Y is the land cover type (either open or low_int for low intensity).
 #'  Each file contains a SpatRaster with the fractional coverage of the land
 #'  cover type for the state on the same grid as the input domain. The csv is
-#'  titled "nlcd_state_total_areas.csv" and provides the total area of each land
+#'  titled "NLCD_state_total_areas.csv" and provides the total area of each land
 #'  cover type in each state.
 #'@examples
 #'library(terra)
@@ -51,7 +51,7 @@
 #'              ncols=diff(range(grid_bbox[,1]))/grid_res, xmin=min(grid_bbox[,1]),
 #'              xmax=max(grid_bbox[,1]), ymin=min(grid_bbox[,2]), ymax=max(grid_bbox[,2]),
 #'              crs=grid_crs)
-#' NLCD_open_and_low_int <- function(nlcd_file="~/../Desktop/nlcd_2019_land_cover_l48_20210604/nlcd_2019_land_cover_l48_20210604.img",
+#' NLCD_open_and_low_int <- function(NLCD_file="~/../Desktop/NLCD_2019_land_cover_l48_20210604/NLCD_2019_land_cover_l48_20210604.img",
 #'                                   State_Tigerlines=vect("~/../Desktop/State_Tigerlines/tl_2018_us_state.shp"),
 #'                                   state_name_list=c("DE","MD","NJ","NY","PA"),
 #'                                   output_directory="~/../Desktop/")
@@ -64,104 +64,144 @@
 # Calculate NLCD fractions for the states in the d03 domain
 ## Finalized: 2023-02-03
 
-NLCD_open_and_low_int <- function(nlcd_file,
+NLCD_open_and_low_int <- function(NLCD_file,
                                   domain,
                                   State_Tigerlines,
                                   state_name_list,
                                   output_directory){
-  XESMF=F
+  starttime <- Sys.time()
+  cat("Starting wastewater sector: NLCD_open_and_low_int")
   ################################################################################
-  #subset NLCD to the appropriate states
-  
-  nlcd <- rast(nlcd_file)
+  #load in the landcover - national land cover database or North
+  #American Land Change Monitoring System (NLCD and NALCMS)
   
   # Make a polygon for the domain (with a small buffer just to be safe)
   crop_area <- vect(ext(domain)+0.5)
   crs(crop_area) <- crs(domain)
-  crop_area_trans <- project(crop_area,crs(nlcd))
   
-  # reproject states to NLCD crs
-  states_trans <- project(State_Tigerlines,crs(nlcd))
-  
-  #crop/mask the nlcd to the states in the domain so that it's a bit more
+  NLCD <- rast(NLCD_file)
+  NLCD_crop_area_trans <- project(crop_area,crs(NLCD))
+  # reproject states to proper CRS
+  NLCD_states_trans <- project(State_Tigerlines,crs(NLCD))
+  #crop to the states in the domain so that it's a bit more
   #manageable in size from the get-go.  Again, add a slight buffer.
-  nlcd <- terra::crop(nlcd,1.01*ext(states_trans))
+  NLCD <- crop(NLCD,1.01*ext(NLCD_states_trans))
   
   ################################################################################
-  # Make new rasters for Developed, Open Space, and Developed, Low Intensity
-  # Do everything one at a time to avoid memory issues
-  nlcd_open <- nlcd
-  values(nlcd_open) <- 0
-  nlcd_open[nlcd==21] <- 1
-  nlcd_low_int <- nlcd
-  values(nlcd_low_int) <- 0
-  nlcd_low_int[nlcd==22] <- 1
-  remove(nlcd)
+  #Make a new raster for only Developed, Open Space, and Developed, Low Intensity (21
+  #and 22 of NLCD)
+  
+  NLCD_suburban <- NLCD
+  NLCD_suburban <- classify(NLCD_suburban,matrix(ncol=3,c(0,20.5,0,
+                                                          20.5,22.5,1,
+                                                          22.5,1000,0),byrow=T))
   ################################################################################
-  # calculate the sum total for each state
+  # calculate the total for each state
   
   #initialize output data frame for state totals
-  area_df <- data.frame("developed_open"=vector(),
-                        "developed_low_intensity"=vector())
+  area_df <- data.frame("developed_open_or_low_intensity"=vector())
   
   for(A in 1:length(state_name_list)){
-    state_sub_poly <- terra::subset(states_trans, states_trans$STUSPS==state_name_list[A])
+    state_sub_poly <- subset(NLCD_states_trans, NLCD_states_trans$STUSPS==state_name_list[A])
     
     #separate states
-    open_d03 <- crop(nlcd_open,state_sub_poly,snap='out')
-    open_d03 <- mask(open_d03,state_sub_poly)
-    low_int_d03 <- crop(nlcd_low_int,state_sub_poly,snap='out')
-    low_int_d03 <- mask(low_int_d03,state_sub_poly)
+    NLCD_suburban_subset <- crop(NLCD_suburban,state_sub_poly,snap='out')
+    NLCD_suburban_subset <- mask(NLCD_suburban_subset,state_sub_poly)
     
-    #calculate total areas per state per type.  N pixels * area per pixel.
-    open_area <- global(open_d03*cellSize(open_d03,unit="km"),sum,na.rm=T)
-    low_int_area <- global(low_int_d03*cellSize(low_int_d03,unit="km"),sum,na.rm=T)
-    area_df <- rbind(area_df,cbind(open_area,low_int_area))
+    #calculate total areas per state.  N pixels * area per pixel.
+    NLCD_subset_area <- global(NLCD_suburban_subset*cellSize(NLCD_suburban_subset,unit="km"),sum,na.rm=T)
+    area_df <- rbind(area_df,NLCD_subset_area)
     rownames(area_df)[A] <- state_name_list[A]
-    cat("Finished processing",state_name_list[A],"\n")
+    cat("Finished processing",state_name_list[A],"landcover at",difftime(Sys.time(),starttime,units = "min"),"minutes since start\n")
   }
-  ################################################################################
-  #project/crop to exactly the domain, then save
   
-  #first aggregate to a similar resolution
-  reprojected_open <- aggregate(nlcd_open,na.rm=T,
-                                fact=floor(res(project(domain,crs(nlcd_open)))/res(nlcd_open)/10)*10,
-                                mean)
-  reprojected_low_int <- aggregate(nlcd_low_int,na.rm=T,
-                                   fact=floor(res(project(domain,crs(nlcd_low_int)))/res(nlcd_low_int)/10)*10,
-                                   mean)
+  ################################################################################
+  #testing different ways to project/agg/crop/mask
+  
+  # domain_trans <- project(as.polygons(domain),crs(NLCD_suburban))
+  # test <- crop(NLCD_suburban,domain_trans,snap="out")
+  # test <- mask(test,domain_trans)
+  # test <- crop(test,state_sub_poly,snap='out')
+  # test <- mask(test,state_sub_poly)
+  # global(test*cellSize(test,unit="km"),sum,na.rm=T)
+  # 
+  # 
+  # 
+  # aggregate, reproject(ngb), then mask
+  # NLCD_suburban_subset3 <- mask(project(aggregate(NLCD_suburban,na.rm=T,
+  #                                                 fact=floor(res(project(domain,crs(NLCD_suburban)))/res(NLCD_suburban)/10)*10,mean),
+  #                                       domain,
+  #                                       method="near"),
+  #                               State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],touch=T)
+  # NLCD_extract <- extract(NLCD_suburban_subset3,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+  #                         weights=T,exact=T,cells=T)
+  # NLCD_suburban_subset3[NLCD_extract[,'cell']] <- NLCD_suburban_subset3[NLCD_extract[,'cell']]*NLCD_extract[,'weight']
+  # NLCD_suburban_subset4 <- mask(project(aggregate(NLCD_suburban,na.rm=T,
+  #                                                 fact=floor(res(project(domain,crs(NLCD_suburban)))/res(NLCD_suburban)/10)*10,mean),
+  #                                       domain,
+  #                                       method="near"),
+  #                               State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],touch=T)
+  # NLCD_extract <- extract(NLCD_suburban_subset4,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+  #                         weights=T,exact=T,cells=T)
+  # NLCD_suburban_subset4[NLCD_extract[,'cell']] <- NLCD_suburban_subset4[NLCD_extract[,'cell']]*NLCD_extract[,'weight']
+  # 
+  # 
+  # 
+  # #alt - try to reproject, then mask, then aggregate
+  # NLCD_suburban_reprojected <- project(NLCD_suburban,crs(domain),method="near")
+  # NLCD_suburban_reprojected2 <- project(NLCD_suburban,crs(domain))
+  # 
+  # NLCD_suburban_subset <- mask(NLCD_suburban_reprojected,
+  #                              State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+  #                              touch=T)
+  # #project to the exact domain (resolution, origin, extent, etc.)
+  # NLCD_suburban_subset <- project(NLCD_suburban_subset,domain)
+  # NLCD_suburban_subset2 <- mask(NLCD_suburban_reprojected2,
+  #                               State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+  #                               touch=T)
+  # NLCD_suburban_subset2 <- project(NLCD_suburban_subset2,domain)
+  # global(NLCD_suburban_subset*cellSize(NLCD_suburban_subset,unit="km"),sum,na.rm=T)
+  # global(NLCD_suburban_subset2*cellSize(NLCD_suburban_subset2,unit="km"),sum,na.rm=T)
+  # global(NLCD_suburban_subset3*cellSize(NLCD_suburban_subset3,unit="km"),sum,na.rm=T)
+  # global(NLCD_suburban_subset4*cellSize(NLCD_suburban_subset4,unit="km"),sum,na.rm=T)
+  ################################################################################
+  #project/crop/mask to exactly the domain, then save
+  
+  #first aggregate to a similar resolution (rounded to the nearest whole number)
+  NLCD_suburban_reprojected <- aggregate(NLCD_suburban,na.rm=T,
+                                         fact=floor(res(project(domain,crs(NLCD_suburban)))/
+                                                      res(NLCD_suburban)/10)*10,mean)
+  
   #then reproject to the exact desired domain using nearest neighbor to have less
   #of an impact on the state/domain totals
-  reprojected_open <- project(reprojected_open,y=domain,method="near")
-  reprojected_low_int <- project(reprojected_low_int,y=domain,method="near")
-  
-  
-  
+  NLCD_suburban_reprojected <- project(NLCD_suburban_reprojected,
+                                        domain)
+
   for(A in 1:length(state_name_list)){
     #mask to just the 1 state
-    subset_open <- mask(reprojected_open,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]])
-    subset_low_int <- mask(reprojected_low_int,State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]])
+    NLCD_suburban_subset <- mask(NLCD_suburban_reprojected,
+                                 State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+                                 touch=T)
+    #account for any pixels that are only partially within the state
+    NLCD_extract <- extract(NLCD_suburban_subset,
+                            State_Tigerlines[State_Tigerlines$STUSPS==state_name_list[A]],
+                            weights=T,exact=T,cells=T)
+    NLCD_suburban_subset[NLCD_extract[,'cell']] <- NLCD_suburban_subset[NLCD_extract[,'cell']]*NLCD_extract[,'weight']
+
     #save
     if(XESMF){
       writeRaster(open_d03,file.path(Output_directory,paste0(state_name_list[A],"_NLCD_open.grd")),overwrite=T)
       writeRaster(low_int_d03,file.path(Output_directory,paste0(state_name_list[A],'_NLCD_low_int.grd')),overwrite=T)
     }else{
-      writeCDF(subset_open,file.path(output_directory,
-                                     paste0(state_name_list[A],"_NLCD_open_regridded.nc")),
-               force_v4=T,
-               overwrite=T)
-      writeCDF(subset_low_int,file.path(output_directory,
-                                        paste0(state_name_list[A],"_NLCD_low_int_regridded.nc")),
+      writeCDF(NLCD_suburban_subset,file.path(output_directory,
+                                              paste0(state_name_list[A],"_NLCD_suburban.nc")),
                force_v4=T,
                overwrite=T)
     }
-    cat("Finished saving",state_name_list[A],"\n")
+    cat("Finished saving",state_name_list[A],"at",difftime(Sys.time(),starttime,units = "min"),"minutes since start\n")
   }
-  colnames(area_df) <- c("open_area","low_int_area")
+  colnames(area_df) <- c("open_or_low_int_area")
   #save state-level totals
-  write.csv(area_df,file.path(output_directory,'nlcd_state_total_areas.csv'))
-  
-  # plot(septic_emiss2,ylim=c(39.5,39.8),xlim=c(-75.85,-75.7))
-  # abline(v=-75.77);abline(v=-75.78);abline(v=-75.79)
-  ## DE and MD overlap here, so they can be compared more clearly.
+  write.csv(area_df,file.path(output_directory,'NLCD_state_total_areas.csv'))
+  cat("Finished wastewater sector: NLCD_open_and_low_int in",difftime(Sys.time(),starttime,units = "min"),"minutes")
 }
