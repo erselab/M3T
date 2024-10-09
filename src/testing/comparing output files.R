7539e19922fb0c75dc6244068dad6357b5dbe0b0
kristian Hajny <kristian.hajny@stonybrook.edu>
2024-10-08 20:35:54 -0400

    Build a simple script to quickly compare output while testing.

---
+++ src/testing/comparing output files.R
@@ -0,0 +1,111 @@
+#quickly compare output files to check if they differ/how much they differ.
+#Only useful for testing after comparing to old raster based code as the raster
+#functions result in some significant differences (effectively noise added each
+#time an area, projection, etc. is applied)
+
+library(terra)
+
+#load in shapefiles to help with context when investigating if needed
+Census_filenames <- "G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Raw_data_rewrite/State_Tigerlines/tl_2019_us_state.shp"
+State_Tigerlines <- vect(Census_filenames[1])
+
+
+test <- function(oldfiles,newfiles){
+  old_data <- rast(oldfiles)
+  new_data <- rast(newfiles)
+  
+  delta <- new_data - old_data
+  
+  if(any(range(na.omit(values(range(delta))))!=0)){
+    assign("old_data",old_data,envir = parent.env(environment()))
+    assign("new_data",new_data,envir = parent.env(environment()))
+    assign("delta",delta,envir = parent.env(environment()))
+    plot(range(delta))
+    cat(basename(old[1]),"sector doesn't match old and new - see old_data, new_data, and delta")
+  }else{
+    cat(basename(old[1]),"sector PERFECTLY matches old and new")
+  }
+}
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="MSW.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="MSW.*\\.nc",full.names = T)
+
+test(old,new)
+#MSW doesn't match, but that's expected.  When building CHECK files, modified to
+#correct an error where facilities that stopped reporting without a valid reason
+#were double counted (in both LMOP and GHGRP)
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_dist.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_dist.*\\.nc",full.names = T)
+
+test(old,new)
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_trans.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_trans.*\\.nc",full.names = T)
+
+test(old,new)
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="stat_comb.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="stat_comb.*\\.nc",full.names = T)
+
+test(old,new)
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wastewater.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wastewater.*\\.nc",full.names = T)
+
+test(old,new)
+#new septic by state is <6E-8 larger emissions for a few pixels (only for PA).
+#I'm not sure why it's only PA, but this must be caused by saving and then
+#reloading the NLCD by state data.  This is necessary given errors I was getting
+#otherwise some runs.
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="SOCCR.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="SOCCR.*\\.nc",full.names = T)
+old_data <- rast(old)
+new_data <- rast(new)
+Freshwater_data <- rast(list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Freshwater.*\\.nc",full.names = T))
+old_data <- c(sum(old_data[[1:10]]),sum(old_data[[11:20]]))
+new_data <- c(new_data[[1]]+Freshwater_data,new_data[[2]]+Freshwater_data)
+delta <- new_data - old_data
+plot(range(delta))
+global(delta,sum)
+#this one has to be more manual as the old approach treated freshwater as part
+#of SOCCR1 and 2 and the new approach saves only the sum across wetland types
+#for SOCCR1, SOCCR2, and Freshwater.  Summing across these before saving likely
+#avoids some rounding error when saving; that would be what's seen here perhaps?
+#Delta of <8E-6 per pixel (+ or -), summed across the domain it adds up to <8E-5
+#with new being higher. It's little in PA or NY, mostly DE and NJ - but that's
+#just where most emissions are.
+
+
+
+
+old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wetcharts.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wetcharts.*\\.nc",full.names = T)
+
+test(old,new)
+
+
+
+
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Freshwater.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Freshwater.*\\.nc",full.names = T)
+# 
+# test(old,new)
+
+
