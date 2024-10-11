78a8f8bcaf6eee1db018b645bcf2b69a77a6fd0c
kristian Hajny <kristian.hajny@stonybrook.edu>
2024-10-10 20:00:55 -0400

    Updates to compare a new output and add a few adiditoinal comments

--- src/testing/comparing output files.R
+++ src/testing/comparing output files.R
@@ -1,7 +1,9 @@
 #quickly compare output files to check if they differ/how much they differ.
 #Only useful for testing after comparing to old raster based code as the raster
 #functions result in some significant differences (effectively noise added each
-#time an area, projection, etc. is applied)
+#time an area, projection, etc. is applied).
+#
+#Comments are based on comparison with output as of Sept 10, 2024
 
 library(terra)
 
@@ -27,43 +29,54 @@ test <- function(oldfiles,newfiles){
   }
 }
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="MSW.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="MSW.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="MSW.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="MSW.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="MSW.*\\.nc",full.names = T)
 
 test(old,new)
 #MSW doesn't match, but that's expected.  When building CHECK files, modified to
 #correct an error where facilities that stopped reporting without a valid reason
-#were double counted (in both LMOP and GHGRP)
+#were double counted (in both LMOP and GHGRP).  There is 1 pixel that's way too
+#high in LMOP (missing in new).
 
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_dist.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_dist.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_dist.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_dist.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="NG_dist.*\\.nc",full.names = T)
 
 test(old,new)
 
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_trans.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_trans.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="NG_trans.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="NG_trans.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="NG_trans.*\\.nc",full.names = T)
 
 test(old,new)
 
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="stat_comb.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="stat_comb.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="stat_comb.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="stat_comb.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="stat_comb.*\\.nc",full.names = T)
 
 test(old,new)
 
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wastewater.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wastewater.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wastewater.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wastewater.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="Wastewater.*\\.nc",full.names = T)
 
 test(old,new)
 #new septic by state is <6E-8 larger emissions for a few pixels (only for PA).
@@ -74,11 +87,14 @@ test(old,new)
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="SOCCR.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="SOCCR.*\\.nc",full.names = T)
+# Freshwater_data <- rast(list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Freshwater.*\\.nc",full.names = T))
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="SOCCR.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="SOCCR.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="SOCCR.*\\.nc",full.names = T)
+Freshwater_data <- rast(list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="Freshwater.*\\.nc",full.names = T))
 old_data <- rast(old)
 new_data <- rast(new)
-Freshwater_data <- rast(list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Freshwater.*\\.nc",full.names = T))
 old_data <- c(sum(old_data[[1:10]]),sum(old_data[[11:20]]))
 new_data <- c(new_data[[1]]+Freshwater_data,new_data[[2]]+Freshwater_data)
 delta <- new_data - old_data
@@ -95,8 +111,10 @@ global(delta,sum)
 
 
 
+# old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wetcharts.*\\.nc",full.names = T)
+# new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wetcharts.*\\.nc",full.names = T)
 old <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_rewrite/",pattern="Wetcharts.*\\.nc",full.names = T)
-new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test",pattern="Wetcharts.*\\.nc",full.names = T)
+new <- list.files("G:/My Drive/Shepson Group Drive/Kris/Philly Inventory/Processed_test2",pattern="Wetcharts.*\\.nc",full.names = T)
 
 test(old,new)
 
