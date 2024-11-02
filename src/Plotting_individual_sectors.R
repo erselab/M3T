dfd6e0636c6f23f5ba6f4004396e704228492f87
kristian Hajny <kristian.hajny@stonybrook.edu>
2024-11-01 20:37:16 -0400

    Use a polygon domain rather than a raster, adding ifs for NA data as needed

--- src/Plotting_individual_sectors.R
+++ src/Plotting_individual_sectors.R
@@ -83,37 +83,86 @@ log_plot <- function(input,title,zlim_min=NULL,zlim_max=NULL,
     outputname <- paste0(plot_directory,filename)
   }
   
-  input <- prep_plot_data(input)
-  
-  if(!is.null(zlim_min)){
-    input[input<zlim_min] <- zlim_min
-  }
-  if(!is.null(zlim_max)){
-    input[input>zlim_max] <- zlim_max
-  }
-  
-  #can't plot an all NA plot.  Would rather plot it as all 0 than not plot it.
+  #can't plot an all NA plot.  Would rather plot it as all 0 (not log) than not
+  #plot it.
   if(all(is.na(values(input)) | values(input)==0)){
-    values(input) <- 0
     plot_type="classes"
-  }
-  
-  png(paste0(outputname,".png"),width = 480*2,height=480*2)
-  plot(input,mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
-       type=plot_type,
-       colNA="black",
-       main=title,
-       plg=list(cex=2,title="log10(nmol/m2/s)",title.cex=2),
-       pax=list(cex.axis=2,line=2),
-       # xlab="Longitude",ylab="Latitude",
-       cex.main=2,cex.axis=2,cex.lab=2,
-       range=c(zlim_min,zlim_max))
-  mtext("Latitude",side = 2,line = 0,cex = 2)
-  mtext("Longitude",side = 1,line = 3.75,cex = 2)
-  plot(project(County_Tigerlines,input),add=T,border="dimgrey",col=NA)
-  plot(project(State_Tigerlines,input),add=T,border="white",lwd=2,col=NA)
-  if(class(focus_city_tigerlines)=="SpatVector"){
-    plot(project(focus_city_tigerlines,input),add=T,border="darkgrey",col=NA)
+    
+    png(paste0(outputname,".png"),width = 480*2,height=480*2)
+    plot(mask(input,domain),mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
+         type=plot_type,
+         colNA="black",
+         main=title,
+         plg=list(cex=2,title="nmol/m2/s",title.cex=2),
+         pax=list(cex.axis=2,line=2),
+         # xlab="Longitude",ylab="Latitude",
+         cex.main=2,cex.axis=2,cex.lab=2)
+    mtext("Latitude",side = 2,line = 0,cex = 2)
+    mtext("Longitude",side = 1,line = 3.75,cex = 2)
+    plot(County_Tigerlines,add=T,border="dimgrey",col=NA)
+    plot(State_Tigerlines,add=T,border="white",lwd=2,col=NA)
+    # plot(Urban_Tigerlines,add=T,border="darkgrey",col=NA)
+    # legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+    #        y=quantile(par('usr')[3:4],0.85),
+    #        legend=c("State","County","Urban"),
+    #        col=c("white","dimgrey","darkgrey"),lty=1,lwd=3,bg="black",xpd=T,
+    #        text.col="white",cex=1.5)
+    legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+           y=quantile(par('usr')[3:4],0.85),
+           legend=c("State","County"),
+           col=c("white","dimgrey"),lty=1,lwd=3,bg="black",xpd=T,
+           text.col="white",cex=1.5)
+  }else{
+    input <- prep_plot_data(input)
+    
+    if(!is.null(zlim_min)){
+      input[input<zlim_min] <- zlim_min
+    }else{
+      zlim_min <- unlist(global(input,min,na.rm=T))
+    }
+    if(!is.null(zlim_max)){
+      input[input>zlim_max] <- zlim_max
+    }else{
+      zlim_max <- unlist(global(input,max,na.rm=T))
+    }
+    
+    if(zlim_max>0){
+      zlim_max=zlim_max*1.00001
+    }else{
+      zlim_max=zlim_max*0.99999
+    }
+    
+    if(zlim_min>0){
+      zlim_min=zlim_min*0.99999
+    }else{
+      zlim_min=zlim_min*1.00001
+    }
+    
+    png(paste0(outputname,".png"),width = 480*2,height=480*2)
+    plot(input,mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
+         type=plot_type,
+         colNA="black",
+         main=title,
+         plg=list(cex=2,title="log10(nmol/m2/s)",title.cex=2),
+         pax=list(cex.axis=2,line=2),
+         # xlab="Longitude",ylab="Latitude",
+         cex.main=2,cex.axis=2,cex.lab=2,
+         range=c(zlim_min,zlim_max))
+    mtext("Latitude",side = 2,line = 0,cex = 2)
+    mtext("Longitude",side = 1,line = 3.75,cex = 2)
+    plot(County_Tigerlines,add=T,border="dimgrey",col=NA)
+    plot(State_Tigerlines,add=T,border="white",lwd=2,col=NA)
+    # plot(Urban_Tigerlines,add=T,border="darkgrey",col=NA)
+    # legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+    #        y=quantile(par('usr')[3:4],0.85),
+    #        legend=c("State","County","Urban"),
+    #        col=c("white","dimgrey","darkgrey"),lty=1,lwd=3,bg="black",xpd=T,
+    #        text.col="white",cex=1.5)
+    legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+           y=quantile(par('usr')[3:4],0.85),
+           legend=c("State","County"),
+           col=c("white","dimgrey"),lty=1,lwd=3,bg="black",xpd=T,
+           text.col="white",cex=1.5)
   }
   dev.off()
   
@@ -171,33 +220,87 @@ not_log_plot <- function(input,title,zlim_min=NULL,zlim_max=NULL,
     outputname <- paste0(plot_directory,filename)
   }
   
-  #Here just set 0 values to NA so that colNA applies
-  input[values(input)==0] <- NA
-  
-  #can't plot an all NA plot.  Would rather plot it as all 0 than not plot it.
   if(all(is.na(values(input)) | values(input)==0)){
-    values(input) <- 0
     plot_type="classes"
-  }
-  
-  png(paste0(outputname,".png"),width = 480*2,height=480*2)
-  par(mar=c(5, 4, 4, 2) + 0.1 + c(0,1,2,1))
-  plot(input,mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
-       # col=timPalette(),
-       type=plot_type,
-       colNA="black",
-       main=title,
-       plg=list(cex=2,title="nmol/m2/s",title.cex=2),
-       pax=list(cex.axis=2),
-       # xlab="Longitude",ylab="Latitude",
-       cex.main=2,cex.axis=2,cex.lab=2,
-       range=c(zlim_min,zlim_max))
-  mtext("Latitude",side = 2,line = 0,cex = 2)
-  mtext("Longitude",side = 1,line = 3.75,cex = 2)
-  plot(project(County_Tigerlines,input),add=T,border="dimgrey",col=NA)
-  plot(project(State_Tigerlines,input),add=T,border="white",lwd=2,col=NA)
-  if(class(focus_city_tigerlines)=="SpatVector"){
-    plot(project(focus_city_tigerlines,input),add=T,border="darkgrey",col=NA)
+    
+    png(paste0(outputname,".png"),width = 480*2,height=480*2)
+    plot(mask(input,domain),mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
+         type=plot_type,
+         colNA="black",
+         main=title,
+         plg=list(cex=2,title="nmol/m2/s",title.cex=2),
+         pax=list(cex.axis=2,line=2),
+         # xlab="Longitude",ylab="Latitude",
+         cex.main=2,cex.axis=2,cex.lab=2)
+    mtext("Latitude",side = 2,line = 0,cex = 2)
+    mtext("Longitude",side = 1,line = 3.75,cex = 2)
+    plot(County_Tigerlines,add=T,border="dimgrey",col=NA)
+    plot(State_Tigerlines,add=T,border="white",lwd=2,col=NA)
+    # plot(Urban_Tigerlines,add=T,border="darkgrey",col=NA)
+    # legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+    #        y=quantile(par('usr')[3:4],0.85),
+    #        legend=c("State","County","Urban"),
+    #        col=c("white","dimgrey","darkgrey"),lty=1,lwd=3,bg="black",xpd=T,
+    #        text.col="white",cex=1.5)
+    legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+           y=quantile(par('usr')[3:4],0.85),
+           legend=c("State","County"),
+           col=c("white","dimgrey"),lty=1,lwd=3,bg="black",xpd=T,
+           text.col="white",cex=1.5)
+  }else{
+    if(!is.null(zlim_min)){
+      input[input<zlim_min] <- zlim_min
+    }else{
+      zlim_min <- unlist(global(input,min,na.rm=T))
+    }
+    if(!is.null(zlim_max)){
+      input[input>zlim_max] <- zlim_max
+    }else{
+      zlim_max <- unlist(global(input,max,na.rm=T))
+    }
+    
+    if(zlim_max>0){
+      zlim_max=zlim_max*1.00001
+    }else{
+      zlim_max=zlim_max*0.99999
+    }
+    
+    if(zlim_min>0){
+      zlim_min=zlim_min*0.99999
+    }else{
+      zlim_min=zlim_min*1.00001
+    }
+    
+    #Here just set 0 values to NA so that colNA applies
+    input[values(input)==0] <- NA
+    
+    
+    png(paste0(outputname,".png"),width = 480*2,height=480*2)
+    plot(input,mar=c(3.1, 3.1, 2.1, 7.1)+c(0,0,7,2),
+         # col=timPalette(),
+         type=plot_type,
+         colNA="black",
+         main=title,
+         plg=list(cex=2,title="nmol/m2/s",title.cex=2),
+         pax=list(cex.axis=2),
+         # xlab="Longitude",ylab="Latitude",
+         cex.main=2,cex.axis=2,cex.lab=2,
+         range=c(zlim_min,zlim_max))
+    mtext("Latitude",side = 2,line = 0,cex = 2)
+    mtext("Longitude",side = 1,line = 3.75,cex = 2)
+    plot(County_Tigerlines,add=T,border="dimgrey",col=NA)
+    plot(State_Tigerlines,add=T,border="white",lwd=2,col=NA)
+    # plot(Urban_Tigerlines,add=T,border="darkgrey",col=NA)
+    # legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+    #        y=quantile(par('usr')[3:4],0.85),
+    #        legend=c("State","County","Urban"),
+    #        col=c("white","dimgrey","darkgrey"),lty=1,lwd=3,bg="black",xpd=T,
+    #        text.col="white",cex=1.5)
+    legend(x=par('usr')[1] - diff(par('usr')[1:2])*0.05,
+           y=quantile(par('usr')[3:4],0.85),
+           legend=c("State","County"),
+           col=c("white","dimgrey"),lty=1,lwd=3,bg="black",xpd=T,
+           text.col="white",cex=1.5)
   }
   dev.off()
 }
